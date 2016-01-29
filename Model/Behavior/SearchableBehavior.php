0dba46002049eabd6189020231ece36922b243fa
Kousuke Taekuchi <kousuketakeuchi@gmail.com>
2016-01-29 21:49:27 +0900

    Fix duplicate utility from Stinrg to CakeText.

--- Model/Behavior/SearchableBehavior.php
+++ Model/Behavior/SearchableBehavior.php
@@ -355,9 +355,9 @@ class SearchableBehavior extends ModelBehavior {
  */
 	protected function _connectedLike($value, $field, $fieldName, $likeMethod = 'LIKE') {
 		$or = array();
-		$orValues = String::tokenize($value, $field['connectorOr']);
+		$orValues = CakeText::tokenize($value, $field['connectorOr']);
 		foreach ($orValues as $orValue) {
-			$andValues = String::tokenize($orValue, $field['connectorAnd']);
+			$andValues = CakeText::tokenize($orValue, $field['connectorAnd']);
 			$and = array();
 			foreach ($andValues as $andValue) {
 				$and[] = array($fieldName . " " . $likeMethod => $field['before'] . $andValue . $field['after']);
